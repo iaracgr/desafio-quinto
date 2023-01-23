8d731ea58cf025fef980fad7d910c099cd8a1cf7
Camila Gonzalez <gcamila.iara@gmail.com>
2023-01-23 12:25:02 -0300

    spring is here

--- src/main/java/com/gonzalez/desafioquinto/DesafioquintoApplication.java
+++ src/main/java/com/gonzalez/desafioquinto/DesafioquintoApplication.java
@@ -2,10 +2,17 @@ package com.gonzalez.desafioquinto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DesafioquintoApplication {
 
+	@GetMapping("/")
+	String home() {
+		return "Spring is here!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioquintoApplication.class, args);
 	}
